5805a9637aaef51373376a39d2979dfa84bb107b
Florian Inard <Katravan>
2015-05-22 08:22:37 +0000

    Modif CarreauPropriete.java

--- src/monopoly/CarreauPropriete.java
+++ src/monopoly/CarreauPropriete.java
@@ -1,27 +1,28 @@
 package monopoly;
 
 public abstract class CarreauPropriete extends Carreau {
-	private int loyerBase;
-	private int prixAchat;
+	private final int loyerBase;
+	private final int prixAchat;
 	private Joueur proprietaire;
 
+        public CarreauPropriete(int numero, String nomCarreau, Monopoly monopoly,int prixAchat,int loyerBase) {
+            super(numero,nomCarreau,monopoly);
+            this.prixAchat = prixAchat;
+            this.loyerBase = loyerBase;
+        }
+        
+        
 	public Joueur getProprietaire() {
 		return this.proprietaire;
 	}
 
-	public int calculLoyer() {
-		throw new UnsupportedOperationException();
-	}
+        private void setProprietaire(Joueur proprietaire) {
+            this.proprietaire = proprietaire;
+        }
+
+	public abstract int calculLoyer() ;
 
 	public void achatPropriete() {
-		throw new UnsupportedOperationException();
-	}
-
-	public int getPrixAchat() {
-		return this.prixAchat;
-	}
-
-	private void setProprietaire(Joueur j) {
-		throw new UnsupportedOperationException();
+		
 	}
 }
