5decdd47161495a47614ae3e9ce8643fc0067acc
novelm <novelm@pc-dg-039-13.iut2.upmf-grenoble.fr>
2015-05-22 15:55:51 +0200

    Modif of ProprieteAConstruire.java kayak

--- src/monopoly/ProprieteAConstruire.java
+++ src/monopoly/ProprieteAConstruire.java
@@ -15,22 +15,25 @@ public class ProprieteAConstruire extends CarreauPropriete {
 
     @Override
     public void achatPropriete() {
-        this.monopoly.inter.afficher("Voulez-vous acheter cette propriété ?");
-        Boolean b = this.monopoly.inter.lireBoolean();
 
-        if (b == true) {
-            if (this.prixAchat > this.proprietaire.getCash()) {
-                this.monopoly.inter.afficher("Vous ne possédez pas assez d'argent pour acheter cette propriété");
-                this.monopoly.inter.afficher("Il vous manque " + (this.prixAchat - this.proprietaire.getCash()) + " euros pour effectuer cette action");
-            } else {
-                this.monopoly.inter.afficher("Nom : "  + this.nomCarreau + "   " + "Groupe : " + this.getGroupe());
-                this.monopoly.inter.afficher("");
+        if (this.prixAchat > this.proprietaire.getCash()) {
+            this.monopoly.inter.afficher("Vous ne possédez pas assez d'argent pour acheter cette propriété");
+            this.monopoly.inter.afficher("Il vous manque " + (this.prixAchat - this.proprietaire.getCash()) + " euros pour effectuer cette action");
+        } else {
+            this.monopoly.inter.afficher("Nom : " + this.nomCarreau + "   " + "Groupe : " + this.getGroupe());
+            this.monopoly.inter.afficher("Coût de la propriété : " + this.prixAchat);
+            this.monopoly.inter.afficher("Voulez-vous acheter cette propriété ?");
+            Boolean b = this.monopoly.inter.lireBoolean();
+            if (b == true) {
+
             }
+
         }
     }
 
     @Override
-    public int calculLoyer(Joueur j) {
+    public int calculLoyer(Joueur j
+    ) {
         return 0;
     }
 
@@ -49,7 +52,7 @@ public class ProprieteAConstruire extends CarreauPropriete {
     public int getNbHotels() {
         return this.nbHotels;
     }
-    
+
     public Groupe getGroupe() {
         return this.groupePropriete;
     }
