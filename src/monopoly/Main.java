1777dbecf2502946ebc49d04b71f8847041442bc
rogeri <rogeri@pc-dg-039-13.iut2.upmf-grenoble.fr>
2015-05-22 10:05:58 +0200

    Includes of data.txt and reading system.

---
+++ src/monopoly/Main.java
@@ -0,0 +1,9 @@
+package monopoly;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Monopoly m = new Monopoly("src/data.txt");
+	}
+
+}
--- src/monopoly/Monopoly.java
+++ src/monopoly/Monopoly.java
@@ -1,75 +1,128 @@
 package monopoly;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Monopoly {
-	private int nbMaisons = 32;
-	private int nbHotels = 12;
-	private Carreau carreaux;
-	private ArrayList<Joueur> joueurs;
-	public Interface interface_8;
 
-	public int[] jetDeDes() {
-		throw new UnsupportedOperationException();
-	}
+    private int nbMaisons = 32;
+    private int nbHotels = 12;
+    private Carreau carreaux;
+    private ArrayList<Joueur> joueurs;
+    public Interface inter;
 
-	public Joueur joueurSuivant() {
-		throw new UnsupportedOperationException();
-	}
+    public Monopoly(String dataFilename) {
+        buildGamePlateau(dataFilename);
+    }
 
-	public int getNbMaison() {
-		throw new UnsupportedOperationException();
-	}
+    private void buildGamePlateau(String dataFilename) {
+        try {
+            ArrayList<String[]> data = readDataFile(dataFilename, ",");
 
-	public void afficherInfosTour(Joueur j, int jet, Carreau c) {
-		throw new UnsupportedOperationException();
-	}
+            //TODO: create cases instead of displaying
+            for (int i = 0; i < data.size(); ++i) {
+                String caseType = data.get(i)[0];
+                if (caseType.compareTo("P") == 0) {
+                    System.out.println("Propriété :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+                } else if (caseType.compareTo("G") == 0) {
+                    System.out.println("Gare :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+                } else if (caseType.compareTo("C") == 0) {
+                    System.out.println("Compagnie :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+                } else if (caseType.compareTo("CT") == 0) {
+                    System.out.println("Case Tirage :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+                } else if (caseType.compareTo("CA") == 0) {
+                    System.out.println("Case Argent :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+                } else if (caseType.compareTo("CM") == 0) {
+                    System.out.println("Case Mouvement :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+                } else {
+                    System.err.println("[buildGamePleateau()] : Invalid Data type");
+                }
+            }
 
-	public int calculTotalDes(int[] des) {
-		throw new UnsupportedOperationException();
-	}
+        } catch (FileNotFoundException e) {
+            System.err.println("[buildGamePlateau()] : File is not found!");
+        } catch (IOException e) {
+            System.err.println("[buildGamePlateau()] : Error while reading file!");
+        }
+    }
 
-	public void saisir() {
-		throw new UnsupportedOperationException();
-	}
+    private ArrayList<String[]> readDataFile(String filename, String token) throws FileNotFoundException, IOException {
+        ArrayList<String[]> data = new ArrayList<String[]>();
 
-	public ArrayList<Joueur> getJoueurs() {
-		throw new UnsupportedOperationException();
-	}
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            data.add(line.split(token));
+        }
+        reader.close();
 
-	public void jouerUnCoup(Joueur j) {
-		throw new UnsupportedOperationException();
-	}
+        return data;
+    }
 
-	public void lancerDésAvancer() {
-		throw new UnsupportedOperationException();
-	}
+    public int[] jetDeDes() {
+        throw new UnsupportedOperationException();
+    }
 
-	public boolean demanderConstruction() {
-		throw new UnsupportedOperationException();
-	}
+    public Joueur joueurSuivant() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void achatMaison(int nb, int prix) {
-		throw new UnsupportedOperationException();
-	}
+    public int getNbMaison() {
+        throw new UnsupportedOperationException();
+    }
 
-	public void achatHotel(int prix) {
-		throw new UnsupportedOperationException();
-	}
+    public void afficherInfosTour(Joueur j, int jet, Carreau c) {
+        throw new UnsupportedOperationException();
+    }
 
-	public void setMaison(int nb) {
-		throw new UnsupportedOperationException();
-	}
+    public int calculTotalDes(int[] des) {
+        throw new UnsupportedOperationException();
+    }
 
-	public boolean verifieAchat(int nbConstruction, int cashJoueur, int maisonBanque) {
-		throw new UnsupportedOperationException();
-	}
+    public void saisir() {
+        throw new UnsupportedOperationException();
+    }
 
-	public int getNbMaisons() {
-		return this.nbMaisons;
-	}
+    public ArrayList<Joueur> getJoueurs() {
+        throw new UnsupportedOperationException();
+    }
 
-	public int getNbHotels() {
-		return this.nbHotels;
-	}
+    public void jouerUnCoup(Joueur j) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void lancerDésAvancer() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean demanderConstruction() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void achatMaison(int nb, int prix) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void achatHotel(int prix) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setMaison(int nb) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean verifieAchat(int nbConstruction, int cashJoueur, int maisonBanque) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getNbMaisons() {
+        return this.nbMaisons;
+    }
+
+    public int getNbHotels() {
+        return this.nbHotels;
+    }
 }
