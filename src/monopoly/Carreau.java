eccf83237ac883be676c79465141795125e44c47
Florian Inard <Katravan>
2015-05-22 08:19:54 +0000

    Modif Carreau.java

--- src/monopoly/Carreau.java
+++ src/monopoly/Carreau.java
@@ -4,4 +4,13 @@ public abstract class Carreau {
 	private int numero;
 	private String nomCarreau;
 	private Monopoly monopoly;
+
+    public Carreau(int numero, String nomCarreau, Monopoly monopoly) {
+        this.numero = numero;
+        this.nomCarreau = nomCarreau;
+        this.monopoly = monopoly;
+    }
+        
+
+    
 }
