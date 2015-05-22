2c843cf7cef9ce64b9d721998f226632a4224e05
Florian Inard <Katravan>
2015-05-22 08:16:40 +0000

    Modif Joueur.java

--- src/monopoly/Joueur.java
+++ src/monopoly/Joueur.java
@@ -1,31 +1,83 @@
 package monopoly;
 
+
+import java.util.ArrayList;
+
 public class Joueur {
+        
+        /* Attributs */
 	private String nomJoueur;
 	private int cash = 1500;
-	private java.util.ArrayList<ProprieteAConstruire> proprietes;
-	private Carreau positionCourante;
-	private java.util.ArrayList<Gare> gares;
+	private ArrayList<ProprieteAConstruire> proprietes;
+	private Carreau position;
+	private ArrayList<Gare> gares;
 	private Monopoly monopoly;
-	private java.util.ArrayList<Compagnie> compagnies;
+	private ArrayList<Compagnie> compagnies;
+        
+        /* Constructeur */
+        // Changer le carreau de départ, plus en paramètre mais direct dans constructeur
+        Joueur (String nom, Monopoly monopoly, Carreau c) {
+            nomJoueur = nom;
+            this.monopoly = monopoly;
+            position = c;
+            proprietes = new ArrayList<>();
+            gares = new ArrayList<>();
+            compagnies = new ArrayList<>();
+        }
+        
+        
+        /* getters */
 
+        public int getCash() {
+            return cash;
+        }
+        
+        public int getNbGares() {
+            return gares.size();
+        }
+        
+        public int getNbCompagnies() {
+            return compagnies.size();
+        }
+        
+        public ArrayList<ProprieteAConstruire> getProprietes() {
+            return proprietes;
+        }
+        
+        public ArrayList<Gare> getGares() {
+            return gares;
+        }
+        
+        public ArrayList<Compagnie> getCompagnies() {
+            return compagnies;
+        }
+        
+        
+        /* Autres méthodes */
+        
+       
+        /* transferts d'argent */
 	public void recevoirLoyer(int l) {
-		throw new UnsupportedOperationException();
+		cash = cash + l;
 	}
 
 	public void payerLoyer(int l) {
-		throw new UnsupportedOperationException();
+		cash = cash - l;
 	}
-
+        
+	public void payer(int l) {
+		cash = cash - l;
+	}
+        
+        /* Deplacement */
+        // Voir comment changer la position par rapport au numéro du carreau
 	public Carreau avancer(int jet) {
-		throw new UnsupportedOperationException();
+                throw new UnsupportedOperationException();
 	}
 
-	public void payer() {
-		throw new UnsupportedOperationException();
-	}
 
-	public void afficherInfosJoueur(Joueur j) {
+        /* Affichage */
+       	public void afficherInfosJoueur(Joueur j) {
 		throw new UnsupportedOperationException();
 	}
 
@@ -33,15 +85,4 @@ public class Joueur {
 		throw new UnsupportedOperationException();
 	}
 
-	public int getNbGares() {
-		throw new UnsupportedOperationException();
-	}
-
-	public int getCash() {
-		return this.cash;
-	}
-
-	public void addPropriete(CarreauPropriete c) {
-		throw new UnsupportedOperationException();
-	}
 }
