4b411114e9e388de7570e760104ee5c16e35c3b2
novelm <novelm@pc-dg-039-13.iut2.upmf-grenoble.fr>
2015-05-22 15:13:25 +0200

    Modif of CarreauPropriete.java

--- src/monopoly/CarreauPropriete.java
+++ src/monopoly/CarreauPropriete.java
@@ -2,12 +2,14 @@ package monopoly;
 
 public abstract class CarreauPropriete extends Carreau {
 
+    private final int loyerBase;
     private final int prixAchat;
     private Joueur proprietaire;
 
-    public CarreauPropriete(int numero, String nomCarreau, Monopoly monopoly, int prixAchat) {
+    public CarreauPropriete(int numero, String nomCarreau, Monopoly monopoly, int prixAchat, int loyerBase) {
         super(numero, nomCarreau, monopoly);
         this.prixAchat = prixAchat;
+        this.loyerBase = loyerBase;
     }
 
     public Joueur getProprietaire() {
@@ -18,9 +20,25 @@ public abstract class CarreauPropriete extends Carreau {
         this.proprietaire = proprietaire;
     }
 
-    public abstract int calculLoyer(Joueur j);
+    public abstract int calculLoyer();
+
+    public int getPrixAchat() {
+        return prixAchat;
+    }
 
     public void achatPropriete() {
+        this.monopoly.inter.afficher("Voulez-vous acheter cette propriété ?");
+        Boolean b = this.monopoly.inter.lireBoolean();
 
+        if (b == true) {
+            if (this.prixAchat > this.proprietaire.getCash()) {
+                this.monopoly.inter.afficher("Vous ne possédez pas assez d'argent pour acheter cette propriété");
+                this.monopoly.inter.afficher("Il vous manque " + (this.prixAchat - this.proprietaire.getCash()) + " euros pour effectuer cette action");
+            } else {
+                this.monopoly.inter.afficher("Nom : "  + this.nomCarreau + "   " + "Groupe : " + this.getGroupe());
+                this.monopoly.inter.afficher("");
+            }
+        }
     }
+
 }
