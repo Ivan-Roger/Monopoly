e0611d4c397e55a8096c015b798932663b66b4f2
novelm <novelm@pc-dg-037-02.iut2.upmf-grenoble.fr>
2015-06-03 11:13:15 +0200

    carte

--- src/monopoly/CarteMouvement.java
+++ src/monopoly/CarteMouvement.java
@@ -3,15 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package monopoly;
 
 /**
  *
  * @author germamax
  */
-public class CarteMouvement extends Carte{
-    private final int mouvement;
+public class CarteMouvement extends Carte {
+
+    private int mouvement;
     private final String typeMouv;
 
     public CarteMouvement(int mouvement, String typeMouv, String description, int id, Monopoly monopoly) {
@@ -20,17 +20,26 @@ public class CarteMouvement extends Carte{
         this.typeMouv = typeMouv;
     }
 
- 
-
-
-    
     @Override
     public void action(Joueur j) {
         if (typeMouv == "relatif") {
+            if (j.getPosition().getId() + this.mouvement > 39) {
+                j.setPosition(super.monopoly.getCarreau(j.getPosition().getId() + this.mouvement - 39));
+                j.recevoirArgent(200);
+            }
             j.setPosition(super.monopoly.getCarreau(j.getPosition().getId() + this.mouvement));
         } else {
-            j.setPosition(super.monopoly.getCarreau(mouvement));
+            if (mouvement > 0 && j.getPosition().getId() > mouvement) {
+                j.setPosition(super.monopoly.getCarreau(mouvement));
+                j.recevoirArgent(200);
+            } else if (mouvement > 0 && j.getPosition().getId() < mouvement) {
+                j.setPosition(super.monopoly.getCarreau(mouvement));
+            } else {
+                mouvement = mouvement * -1;
+                j.setPosition(super.monopoly.getCarreau(mouvement));
+            }
         }
+
     }
-    
+
 }
