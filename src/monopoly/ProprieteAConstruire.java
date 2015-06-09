afba62e6df6bc2fdb88a696cb00d3d66cbbd78b0
Florian Inard <Katravan>
2015-06-09 12:37:44 +0000

    ProprieteAConstruire.java edited

--- src/monopoly/ProprieteAConstruire.java
+++ src/monopoly/ProprieteAConstruire.java
@@ -92,5 +92,59 @@ public class ProprieteAConstruire extends CarreauPropriete {
             }
 
         }
+        
+        if ( (p.getCash() < groupePropriete.getPrixAchatMaison()) || ( (nbMaisons == nbMaisonsMax) && (p.getCash() < groupePropriete.getPrixAchatHotel()) ) ) {
+            peutConstruire[2] = false;  // Le joueur possède assez d'argent ou non pour construire une maison ou un hôtel
+        }
+        if (monopoly.getNbMaisonsRestantes() == 0) {
+            peutConstruire[3] = false;  // Il reste des maisons constructibles sur le plateau ou non
+        }   
+        if (monopoly.getNbHotelsRestants() == 0) {
+            peutConstruire[4] = false;  // Il reste des hôtels constructibles sur le plateau ou non
+        }   
+            
+            
+        if (!peutConstruire[0]) {
+            monopoly.inter.afficher("Vous n'êtes pas propriétaire de tous les terrains de ce groupe. Vous ne pouvez pas construire.");
+        } else if (!peutConstruire[1]) {
+            monopoly.inter.afficher("Tous les terrains de ce groupe possèdent déjà le nombre maximal de maisons et d'hôtels. Vous ne pouvez pas construire.");
+        } else if (!peutConstruire[2]) {
+            monopoly.inter.afficher("Vous n'avez pas assez d'argent pour construire sur les terrains de ce groupe.");
+        } else if (!peutConstruire[3]) {
+            monopoly.inter.afficher("Il n'y a plus aucunes maisons constructibles sur le plateau. Vous ne pouvez pas construire.");
+        } else if (!peutConstruire[4]) {
+            monopoly.inter.afficher("Il n'y a plus aucuns hôtels constructibles sur le plateau. Vous ne pouvez pas construire.");
+        } else {
+            monopoly.inter.afficherEtatConstructions(groupePropriete);
+            
+            
+            // Calcul du nombre de maisons et d'hôtels constructibles en fonction du nombre restant sur la plateau et de l'argent du joueur.
+            int compteurCash = p.getCash();
+            int maisons = nbMaisonsMax * groupePropriete.getNbProprietes() - groupePropriete.getNbMaisonsGroupe();
+            int hotels = nbHotelsMax * groupePropriete.getNbProprietes() - groupePropriete.getNbHotelsGroupe();
+            if (monopoly.getNbMaisonsRestantes() < maisons) {
+                maisons = monopoly.getNbMaisonsRestantes();
+            }
+            if (monopoly.getNbHotelsRestants() < hotels) {
+                hotels = monopoly.getNbHotelsRestants();
+            }
+            
+            int maisonsConstructibles = -1, hotelsConstructibles = -1;
+            while (compteurCash > 0) {
+                if (maisons > 0) {
+                    compteurCash = compteurCash - groupePropriete.getPrixAchatMaison();
+                    maisons--;
+                    maisonsConstructibles++;
+                }
+                if (maisons == 0 && hotels > 0) {
+                    compteurCash = compteurCash - groupePropriete.getPrixAchatHotel();
+                    maisons--;
+                    hotelsConstructibles++;
+                }
+            }
+            monopoly.inter.afficherConstructionsPossibles(maisonsConstructibles, hotelsConstructibles, groupePropriete);
+            
+        }
+        
     }
 }
